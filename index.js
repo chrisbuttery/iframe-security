d06a91dbb1f6b5c64a9dd0bdf1511f42c7331184
chrisbuttery <info@chrisbuttery.com>
2013-10-14 12:41:27 +1100

    Added comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-module.exports = function iframeSecurity(){
+/**
+ * iframe-security
+ * if the site location isnt top, hide the html
+ * otherwise display it
+ */
+module.exports = function(){
   if (window.self === window.top) {
-    var el = document.getElementsByTagName('body')[0];
+    var el = document.getElementsByTagName('html')[0];
     el.classList.add('block');
   } else {
     window.top.location = window.self.location;
